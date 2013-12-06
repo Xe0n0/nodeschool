560cb0a2c4c7bf2e4c1670b3943b2e7cd4dd7bea
wuhaotian <thelongestmailaccounteverexist@gmail.com>
2013-12-07 01:06:43 +0800

    lines.js

---
+++ stream-adventure/lines.js
@@ -0,0 +1,6 @@
+var isOdd = true;
+process.stdin.pipe(require('split')()).pipe(require('through')(function(line){
+    if (isOdd) this.queue(line.toString().toLowerCase() + '\n');
+    else this.queue(line.toString().toUpperCase() + '\n');
+    isOdd = !isOdd;
+  }, function(){this.queue(null)})).pipe(process.stdout);
