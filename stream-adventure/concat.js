fc4663f7741aaa468a60b7ad3677b03d60a6658b
wuhaotian <thelongestmailaccounteverexist@gmail.com>
2013-12-07 11:17:59 +0800

    experiment with concat-stream.js

--- stream-adventure/concat.js
+++ stream-adventure/concat.js
@@ -1,6 +1,11 @@
+//could also listen to process.stdin end event and use getBody() from concat
+//but concat's finish event not work, have to use concat's end event! WTF!
 var concat = require('concat-stream');
-
-process.stdin.pipe(concat(function(body) {
-  var rs = body.toString().split('').reverse().join('');
-  console.log(rs);
-}));
+cc = concat(function(body) {
+    var rs = body.toString().split('').reverse().join('');
+      console.log(rs);
+});
+cc.on('end', function() {
+  //console.log('event');
+});
+process.stdin.pipe(cc);
